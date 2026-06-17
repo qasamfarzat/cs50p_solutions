97ef3e981e6a477650e7f8ecbc64bd0d5cceeb7a
qasemfarzat <qfarzat96@gmail.com>
2026-06-17 13:56:06 +0200

    add new one

---
+++ problem_set_1/interpreter.py
@@ -0,0 +1,12 @@
+x, y, z = input("Expression: ").split()
+x = float(x)
+z = float(z)
+if y == "+":
+    print(f"{x + z:.1f}")
+elif y == "-":
+    print(f"{x - z:.1f}")
+elif y == "*":
+    print(f"{x * z:.1f}")
+elif y == "/":
+    print(f"{x / z:.1f}")
+    
